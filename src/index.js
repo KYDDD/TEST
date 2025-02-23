82c6ef9659c865d60dacfe2a0e9e5a94bee4fafe
yeono <riderkim88@naver.com>
2025-02-23 21:28:37 +0900

    자바스크립트 배열 내장함수

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let colors = [
+  { id: 1, color: "green" },
+  { id: 2, color: "blue" },
+  { id: 3, color: "purple" },
+  { id: 4, color: "yellow" },
+];
+
+let sliceArray = colors.slice(1, 3);
+console.log(sliceArray);
