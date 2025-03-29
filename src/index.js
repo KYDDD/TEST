c525cf12ec291bbc6b3e7a262875a8d879ba33b2
yeono <126737362+KYDDD@users.noreply.github.com>
2025-03-29 21:49:16 +0900

    자바스크립트 예습

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-let colors = [
-  { id: 1, color: "green" },
-  { id: 2, color: "blue" },
-  { id: 3, color: "purple" },
-  { id: 4, color: "yellow" },
-];
-
-let sliceArray = colors.slice(1, 3);
-console.log(sliceArray);
+console.log("start VSCode");
+console.log("Hello world");
