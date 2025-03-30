7e858dd6c872e5be084ff1ab7f5ff5d51b849fc3
yeono <126737362+KYDDD@users.noreply.github.com>
2025-03-30 21:31:02 +0900

    js학습중

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-console.log("start VSCode");
-console.log("Hello world");
+console.log("Hello World!");
+console.log(1);
+console.log(true);
+console.log({ name: "홍길동", age: 20, married: false });
+console.log("Hi!", 100, true, [1, 2, 3]);
